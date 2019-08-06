d6476654586a5f39f6e0b0ae42de866b66efbe7a
Rickrhone <rickrhone@gmail.com>
2019-08-06 15:06:37 -0400

    Aug 2 2019 Labs - Arrays, Objects, Functions, and This keyword

--- ClassWork/js/Aug62019.js
+++ ClassWork/js/Aug62019.js
@@ -1,6 +1,6 @@
-// Ricardo R. LAB 1
+// ******************** Ricardo R. LAB 1 - Arrays ***************************
 
-console.log("Ricardo Rhone Lab 1 - 8/6/2019")
+console.log("Ricardo Rhone Lab 1 (Arrays) - 8/6/2019")
 // 1. Shopping List Array
 var ShoppingList = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
 console.log("1. Shopping list Array");
@@ -62,8 +62,8 @@ var course = {
 
 };
 
-// LAB 2
-console.log("\nRicardo Rhone Lab 2 - 8/6/2019")
+// ************************* LAB 2 - OBJECTS ****************************
+console.log("\nRicardo Rhone Lab 2 (Objects) - 8/6/2019")
 //Given the following object: 
 var course = {
     name: 'JavaScript Applications',
@@ -130,7 +130,7 @@ console.log("\n7. Get the string listing the OSOptions separated by 'or' ('linux
 console.log(course.preReqs.equipment.OSOptions.join(' or ').toString());
 
 // 8. An array of all the students that are using OSX.
-console.log("\n7. Get an array of all the students that are using OSX.");
+console.log("\n8. Get an array of all the students that are using OSX.");
 
 let studentsUsingOSX = [];
 
@@ -141,3 +141,90 @@ for (let counter = 0; counter < course.students.length; counter++) {
 }
 
 console.log(studentsUsingOSX);
+
+
+// ********************* LAB 3 - Functions Basics ************************************
+console.log("\nRicardo Rhone Lab 3 (Functions Basics) - 8/6/2019");
+
+//Calculating gratuity is a repetitive task, so let's create a couple of functions that do the work for us.
+
+// 1. create a variable titled billAmount and store a random number (ie: 100)
+let billAmount = 100;
+console.log("\n1. create a variable titled billAmount and store a random number (ie: 100)");
+console.log(billAmount);
+
+// 2. create a function titled gratuity() to multiply the value of billAmount by 20% and return the value
+function gratuity(billAmount) {
+    return billAmount * 0.2;
+}
+console.log("\n2. create a function titled gratuity() to multiply the value of billAmount by 20% and return the value");
+console.log(gratuity);
+
+/* 3. create a function titled totalWithGrat() to 
+ * take in the amount as an argument
+ * call the gratutity function
+ * add that to the original bill amount
+ * return the total bill + gratuity
+ * log the total (with gratuity) to the console
+ * append new total to the following phrase: "your total including gratuity is:"
+ * Limitation: You can only invoke the totalWithGrat function when logging the result
+ */
+function totalWithGrat(billAmount) {
+    let totalBillPlusGratuity = billAmount + gratuity(billAmount)
+    console.log("your total including gratuity is: " + totalBillPlusGratuity);
+    return totalBillPlusGratuity;
+}
+console.log("\n3. create a function titled totalWithGrat() and log the result to the console");
+console.log(totalWithGrat(100));
+
+// ********************* LAB 4 - This Keyword ************************************
+console.log("\nRicardo Rhone Lab 4 (This Keyword) - 8/6/2019");
+
+/* 
+Create a single object named slideshow that represents the data and functionality of a picture slideshow. 
+There should be NO VARIABLES OUTSIDE THE OBJECT. 
+*/
+
+let slideshow = {
+    //The object should have properties: 
+
+    // 1. An array called photoList that contains the names of the photos as strings
+    photoList: ['Photo 1', 'Photo 2', 'Photo 3', 'Photo 4', 'Photo 5'],
+
+    // 2. An integer currentPhotoIndex that represents which photo in the photoList is currently displayed
+    currentPhotoIndex: 0,
+
+    // 3. A nextPhoto() function that moves currentPhotoIndex to the next index if there is one, and: logs the current photo name.
+    nextPhoto: function () {
+        for (let counter = 0; this.currentPhotoIndex < this.photoList.length; counter++) {
+
+            // 4. logs the current photo name.
+            console.log(this.photoList[this.currentPhotoIndex])
+            this.currentPhotoIndex++;
+
+        }
+        // 5. Otherwise, log "End of slideshow"
+        console.log("End of slideshow");
+        this.currentPhotoIndex -= 1; // resets to the last index position
+    },
+
+    // 6. A prevPhoto() function that does the same thing, but backwards.
+    prevPhoto: function () {
+        for (let counter = this.photoList.length; this.currentPhotoIndex > 0; counter--) {
+            // 4. logs the current photo name.
+            console.log(this.photoList[this.currentPhotoIndex - 1])
+            this.currentPhotoIndex--;
+        }
+        // Otherwise, log "End of slideshow"
+        console.log("End of slideshow");
+    },
+
+    // 7. A function getCurrentPhoto() that returns the current photo from the list
+    getCurrentPhoto: function () {
+        return this.photoList[this.currentPhotoIndex];
+    }
+}
+
+slideshow.nextPhoto();
+slideshow.prevPhoto();
+console.log(slideshow.getCurrentPhoto());
