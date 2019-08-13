73b1704b12e3c1d1abdad6733a5bbd805e34e492
Rickrhone <rickrhone@gmail.com>
2019-08-13 10:26:50 -0400

    intro to TypeScript

---
+++ ClassWork/TypeScript/main.js
@@ -0,0 +1,9 @@
+var message = "from typeScript 'main.ts'";
+console.log(message);
+{
+    var isDone = false;
+    var decimal = 6.0;
+    var hex = 0xffffff;
+    var binary = 10;
+    var octal = 508;
+}
---
+++ ClassWork/TypeScript/main.ts
@@ -0,0 +1,9 @@
+var message: string = "from typeScript 'main.ts'";
+console.log(message);
+{
+  let isDone: boolean = false;
+  let decimal: number = 6.0;
+  let hex: number = 0xffffff;
+  let binary: number = 0b1010;
+  let octal: number = 0o774;
+}
