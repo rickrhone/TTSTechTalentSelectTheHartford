4fdfb0401e5dc7909187cc2e88ac630b2a3ff4be
Rickrhone <rickrhone@gmail.com>
2019-08-14 13:09:21 -0400

    TypeScript Class example

--- ClassWork/TypeScript/greeting.js
+++ ClassWork/TypeScript/greeting.js
@@ -3,3 +3,20 @@ function sayHello(person) {
 }
 var user = "Teacher McGee";
 document.getElementById("para").innerHTML = sayHello(user);
+//Interfaces
+// interface PersonData {
+//   title: string;
+//   name: string;
+//   salary?: number;
+//   readonly SSN: number;
+// }
+var Employee = /** @class */ (function () {
+    function Employee(name, age) {
+        this.Name = name;
+        this.Age = age;
+    }
+    Employee.prototype.details = function () {
+        return this.Name + " is " + this.Age + " years old.";
+    };
+    return Employee;
+}());
--- ClassWork/TypeScript/greeting.ts
+++ ClassWork/TypeScript/greeting.ts
@@ -1,5 +1,27 @@
-function sayHello(person) {
+function sayHello(person: string): string {
   return "Hello, " + person;
 }
 var user = "Teacher McGee";
 document.getElementById("para").innerHTML = sayHello(user);
+
+//Interfaces
+// interface PersonData {
+//   title: string;
+//   name: string;
+//   salary?: number;
+//   readonly SSN: number;
+// }
+
+class Employee {
+  Name: string;
+  Age: number;
+
+  constructor(name: string, age: number) {
+    this.Name = name;
+    this.Age = age;
+  }
+
+  details(): string {
+    return this.Name + " is " + this.Age + " years old.";
+  }
+}
