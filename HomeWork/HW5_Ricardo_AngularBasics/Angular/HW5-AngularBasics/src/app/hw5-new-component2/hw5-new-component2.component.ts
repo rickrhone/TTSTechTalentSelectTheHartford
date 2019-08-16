6fe3a137d6f6ed9abc8ec568c6df946086ed678b
Rickrhone <rickrhone@gmail.com>
2019-08-16 11:52:08 -0400

    Completed the services and direct injection section of the HW

--- HomeWork/HW5_Ricardo_AngularBasics/Angular/HW5-AngularBasics/src/app/hw5-new-component2/hw5-new-component2.component.ts
+++ HomeWork/HW5_Ricardo_AngularBasics/Angular/HW5-AngularBasics/src/app/hw5-new-component2/hw5-new-component2.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+//import the persin service
+import { PersonService } from "../person.service";
+import { Person } from "../Person";
 
 @Component({
-  selector: 'app-hw5-new-component2',
-  templateUrl: './hw5-new-component2.component.html',
-  styleUrls: ['./hw5-new-component2.component.css']
+  selector: "app-hw5-new-component2",
+  templateUrl: "./hw5-new-component2.component.html",
+  styleUrls: ["./hw5-new-component2.component.css"]
 })
 export class HW5NewComponent2Component implements OnInit {
+  person1: Person;
+  person2: Person;
 
-  constructor() { }
+  constructor(private personservice: PersonService) {}
 
   ngOnInit() {
+    this.person1 = this.personservice.getPerson1();
+    this.person2 = this.personservice.getPerson2();
   }
-
 }
---
+++ HomeWork/HW5_Ricardo_AngularBasics/Angular/HW5-AngularBasics/src/app/person.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { Person } from "./Person";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+
+/* In the Service class, reference the Person class from above. Create a variable of type Person. In the
+constructor for the service give it some default values. */
+export class PersonService {
+  person1: Person;
+  person2: Person;
+  person3: Person; // to be used with Observables
+  constructor() {
+    this.person1 = new Person("Gordon", "Lyft", 26);
+    this.person2 = new Person("Lillian", "Armstrong", 35);
+    this.person3 = new Person("John", "Observable", 56);
+  }
+
+  getPerson1(): Person {
+    return this.person1;
+  }
+  getPerson2(): Person {
+    return this.person2;
+  }
+
+  //Change the service call to return an Obervable rather than a Person object
+  getPerson3(): Observable<Person> {
+    return of(PEOPLE);
+  }
+}
