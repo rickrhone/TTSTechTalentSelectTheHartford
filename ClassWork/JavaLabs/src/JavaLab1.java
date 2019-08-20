62f87eb0ee606ca5d2298692d3ba8b979564adfe
Rickrhone <rickrhone@gmail.com>
2019-08-19 21:08:18 -0400

    Started Java Data Types and Variables - LAB

---
+++ ClassWork/JavaLabs/src/JavaLab1.java
@@ -0,0 +1,12 @@
+public class JavaLab1 {
+// Task 1 - Start with a very basic class with main() method and Hello World! so that it can be run easily
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+// Task 2 - Declare two variables in the class: 1. an integer named methodCount 2. a string variable named className
+    
+
+
+}
