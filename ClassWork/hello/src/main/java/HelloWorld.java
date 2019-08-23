82d6578b62107d3552cd8eaf9b11ae042993ad6a
Rickrhone <rickrhone@gmail.com>
2019-08-23 07:43:48 -0400

    Aug 22, 2019 intro to Java Classwork

--- ClassWork/hello/src/main/java/HelloWorld.java
+++ ClassWork/hello/src/main/java/HelloWorld.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class HelloWorld {
+
+public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println();
+    double x = 0.7;
+    double y = 0.1;
+    System.out.println(x + y);
+
+
+    BigDecimal xbd = new BigDecimal("1.2");
+    BigDecimal ybd = new BigDecimal("1.2");
+    System.out.println(xbd.add(ybd));
+
+    String Name = "Dolly";
+    System.out.printf("Hello %s%n", Name);
+
+//    Scanner input = new Scanner(System.in);
+//    String Line = input.nextLine();
+//    System.out.printf("Hello%s%n", Line);
+
+    double amount = 123467.8901234;
+    NumberFormat nf= NumberFormat.getCurrencyInstance();
+    System.out.println(nf.format(amount));
+
+}
+
 }
