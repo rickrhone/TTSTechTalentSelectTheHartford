9f24f600fb32488a3d160ba65d4563cde96720a3
Rickrhone <rickrhone@gmail.com>
2019-08-25 11:03:10 -0400

    completed the assignment

--- HomeworkAndLabs/HW5_6_and7_Ricardo_Angular/Angular/Prime-Test/src/app/app.component.ts
+++ HomeworkAndLabs/HW5_6_and7_Ricardo_Angular/Angular/Prime-Test/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MenuItem } from "../app/api";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Prime-Test';
+  title = "Prime-Test";
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      { label: "Stats", icon: "fa fa-fw fa-bar-chart" },
+      { label: "Calendar", icon: "fa fa-fw fa-calendar" },
+      { label: "Documentation", icon: "fa fa-fw fa-book" },
+      { label: "Support", icon: "fa fa-fw fa-support" },
+      { label: "Social", icon: "fa fa-fw fa-twitter" }
+    ];
+  }
 }
--- HomeworkAndLabs/HW5_6_and7_Ricardo_Angular/Angular/Prime-Test/src/app/app.module.ts
+++ HomeworkAndLabs/HW5_6_and7_Ricardo_Angular/Angular/Prime-Test/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ButtonModule } from "primeng/button";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { ListboxModule } from "primeng/listbox";
+import { MenuModule, MenuItemContent } from "primeng/menu";
+import { MenuItem } from "primeng/api";
+import { TabMenuModule } from "primeng/tabmenu";
+import { TableModule } from "primeng/table";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextareaModule,
+    ListboxModule,
+    MenuModule,
+    TabMenuModule
+    // TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
