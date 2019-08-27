6db361a436da759972f366a58f8cdb5ee1380c89
Rickrhone <rickrhone@gmail.com>
2019-08-26 21:10:01 -0400

    Created all classes and Interfaces

--- HomeworkAndLabs/JavaLabs/JavaLab2_ClassesAndInterfaces/src/com/tts/oop/model/LibraryItem.java
+++ HomeworkAndLabs/JavaLabs/JavaLab2_ClassesAndInterfaces/src/com/tts/oop/model/LibraryItem.java
@@ -8,19 +8,30 @@ public class LibraryItem {
     private String locationTag; // encoding to locate and store on shelves
     private String shelfLocation;
     private String title;
-    private String[] Author; // Array is used to account for multiple authors
+    private String[] author; // Array is used to account for multiple authors
     private String publisher;
     private String[] genre; // Array is use to account for multiple genres
     private String format;
     private String releaseDate;
 
     // Constructor
+    public LibraryItem() {
+        this.ISBN = 123456789;
+        this.locationTag = "L1-A";
+        this.title = "Default Library Item";
+        this.author = new String[]{"Ricardo Rhone", "Author 2"};
+        this.publisher = "Default Publisher";
+        this.genre = new  String[] {"non-fiction", "Technology"};
+        this.format = "Book";
+        this.releaseDate = "Jan 1, 1999";
+    }
+
     public LibraryItem(int ISBN, String locationTag, String shelfLocation, String title, String[] author, String publisher, String[] genre, String format, String releaseDate) {
         this.ISBN = ISBN;
         this.locationTag = locationTag;
         this.shelfLocation = shelfLocation;
         this.title = title;
-        Author = author;
+        this.author = author;
         this.publisher = publisher;
         this.genre = genre;
         this.format = format;
