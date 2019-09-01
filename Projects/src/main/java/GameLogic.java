91b4559deb1cd8f21d6854a2c38496dad6727305
RickRhone <rickrhone@gmail.com>
2019-09-01 17:53:20 -0400

    Vegas Dice Game

--- Projects/src/main/java/GameLogic.java
+++ Projects/src/main/java/GameLogic.java
@@ -1,4 +1,81 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+// Contains all the methods for the game
 public class GameLogic {
+
+    // Method to create an ArrayList of winning values for the first try - Returns the ArrayList
+    public static List firstTryWinningValues() {
+        List<Integer> firstTryWinningValues = new ArrayList<>();
+        firstTryWinningValues.add(7);
+        firstTryWinningValues.add(11);
+        return firstTryWinningValues;
+    }
+
+    // Method to create an ArrayList of losing values for the first try - Returns the ArrayList
+    public static List firstTryLosingValues() {
+        List<Integer> firstTryLosingValues = new ArrayList<>();
+        firstTryLosingValues.add(2);
+        firstTryLosingValues.add(3);
+        firstTryLosingValues.add(12);
+        return firstTryLosingValues;
+    }
+
+    // Method to simulate a die roll - returns a number between 1 and 6
+    public static int diceValue() { // accessible without instantiation
+        return 1 + (int)(Math.random() * 6);
+    }
+
+    // Method capture the rolls of die 1 and 2 - returns the sum of both dice
+    public static int newRoll() { // accessible without instantiation
+        // Store the rolls of both die
+        int die1Value = diceValue();
+        int die2Value = diceValue();
+        // log the values to the console
+        System.out.println("Die1Value = " + die1Value + " | " + "Die2Value = " + die2Value + " | " + "Sum = " + (die1Value + die2Value));
+        // return the sum of the die values
+        return die1Value + die2Value;
+    }
+
+    // Method to notify the player that they won
+    public static void youWon() { // accessible without instantiation
+        System.out.println("\n         ******************************      ");
+        System.out.println("         *     VICTORY IS YOURS!!!    *      ");
+        System.out.println("         ******************************      ");
+    }
+
+    // Method to notify the player that they loss
+    public static void youLoss() { // accessible without instantiation
+        System.out.println("\n         ************************************* ");
+        System.out.println("         * YOU LOSE - BETTER LUCK NEXT TIME  * ");
+        System.out.println("         *************************************   ");
+    }
+
+    // Method to end the current game
+    public static boolean gameOver() {
+        System.out.println("\n******************** GAME OVER ********************");
+        return true;
+    }
+
+//    // Method to ask the player if they would like to paly again
+//    // not included in the playAgain method because Scanner.next() is "blocking"
+//    public static void playAgainPrompt() {
+//        // Ask the player for input
+//        System.out.print("\nWould you like to play again (Y/N)? ");
+//    }
+
+    // Method to start a new game
+    public static String playAgain() {
+        // Store the players input
+        Scanner sc = new Scanner(System.in);
+        String newGame = sc.nextLine();
+//        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//        String newGame = input.readLine();
+        return newGame;
+    }
+
 }
--- Projects/src/main/java/Main.java
+++ Projects/src/main/java/Main.java
@@ -1,4 +1,96 @@
-package PACKAGE_NAME;
+//Objective: Create a program that prints out whether you won or lost on each roll.
+
+// This class is solely for the execution of the program
+
+import java.util.List;
 
 public class Main {
+
+    public static void main(String[] args) {
+        // Declaring and Storing the winning and loosing values
+        // ArrayList of wining values on the first try
+        List firstTryWinningValues = GameLogic.firstTryWinningValues();
+
+        // ArrayList of loosing values on the first try
+        List firstTryLosingValues = GameLogic.firstTryLosingValues();
+
+        // Declaring the variable to be used to quit the game.
+        String newGame = "Y";
+
+        // Declaring the variable to store the players choice for a new game
+        String playersChoice = "Y";
+
+        // Starting a new game
+        do {
+
+            // Declaring a variable to store the point
+            int point = 0;
+
+            // Declaring and initializing a variable to store the condition in which the player looses starting at roll 2.
+            int lossOnRoll2Plus = 7;
+
+            // Declaring a variable to store the current try
+            int currentTry = 1;
+
+            // Declaring a variable to store when the game has ended
+            boolean endOfGame = false;
+
+            while (!endOfGame) {
+                // Perform the following while the game is in process
+
+                System.out.println("\n***************** Current Try = " + currentTry + " *****************");
+                // First Try - Roll the dice and store the sum of both die
+                int sumOfDieValues = GameLogic.newRoll();
+
+                // Determine if the player won the game on the first try and if so end the game
+                if (firstTryWinningValues.contains(sumOfDieValues)) {
+                    GameLogic.youWon();
+                    endOfGame = GameLogic.gameOver();
+                    // Determine if the player would like to play again
+                    System.out.print("\nWould you like to play again (Y/N)? ");
+                    playersChoice = GameLogic.playAgain();
+                } else if (firstTryLosingValues.contains(sumOfDieValues)) {
+                    // Determine if the player loss the game on the first try and if so end the game
+                    GameLogic.youLoss();
+                    endOfGame = GameLogic.gameOver();
+                    // Determine if the player would like to play again
+                    System.out.print("\nWould you like to play again (Y/N)? ");
+                    playersChoice = GameLogic.playAgain();
+                } else {
+                    // Store the point
+                    point = sumOfDieValues;
+                    System.out.println("POINT = " + point);
+                    currentTry++; // Ends the first try
+
+                    // Starts try 2
+                    do {
+                        System.out.println("\n***************** Current Try = " + currentTry + " *****************");
+                        // Roll again and store the values
+                        sumOfDieValues = GameLogic.newRoll();
+                        // determine if the player won the game on subsequent tries and if so end the game
+                        if (sumOfDieValues == point) {
+                            GameLogic.youWon();
+                            endOfGame = GameLogic.gameOver();
+                            // Determine if the player would like to play again
+                            System.out.print("\nWould you like to play again (Y/N)? ");
+                            playersChoice = GameLogic.playAgain();
+                        } else if (sumOfDieValues == lossOnRoll2Plus) {
+                            // determine if the player loss the game on subsequent tries and if so end the game
+                            GameLogic.youLoss();
+                            endOfGame = GameLogic.gameOver();
+                            // Determine if the player would like to play again
+                            System.out.print("\nWould you like to play again (Y/N)? ");
+                            playersChoice = GameLogic.playAgain();
+                        } else {
+                            currentTry++; // Move to the next try
+                        }
+
+                    } while (!endOfGame); // End the current game
+                }
+
+            }
+
+        } while (newGame.equalsIgnoreCase(playersChoice)); // Game re-entry / exit condition
+    }
+
 }
